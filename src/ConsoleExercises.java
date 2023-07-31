6b7294f8359ddcd31780aff6328cde9eb27347a7
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-07-31 11:36:33 -0500

    commit HelloWorld and ConsoleExercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.format("The approximate value of PI is %.2f%n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+//        System.out.printf("Enter an Integer%n");
+//        int userInt = scanner.nextInt();
+//        System.out.printf("Your number is: %d%n",userInt);
+//
+//        System.out.printf("Enter 3 words%n");
+//        String userString1 = scanner.next();
+//        String userString2 = scanner.next();
+//        String userString3 = scanner.next();
+//        System.out.printf("%s%n%s%n%s%n",userString1, userString2, userString3);
+//
+//        Scanner scanner2 = new Scanner(System.in);
+//        System.out.printf("Enter a Sentence%n");
+//        String userSentence = scanner2.nextLine();
+//        System.out.printf("%s", userSentence);
+
+//        Scanner scanner3 = new Scanner(System.in);
+//        System.out.printf("Enter the length of your room%n");
+//        String length = scanner3.nextLine();
+////        int lengthNum = Integer.parseInt(length);
+//        float lengthNum = Float.parseFloat(length);
+
+//        Scanner scanner4 = new Scanner(System.in);
+//        System.out.printf("Enter the width of your room%n");
+//        String width = scanner4.nextLine();
+////        int widthNum = Integer.parseInt(width);
+//        float widthNum = Float.parseFloat(width);
+//
+//        System.out.printf("Length: %f%nWidth: %f%n", lengthNum, widthNum);
+//        float area = widthNum*lengthNum;
+//        System.out.printf("Area: %f%n", area);
+//        float perimeter = (widthNum*2)+(lengthNum*2);
+//        System.out.printf("Perimeter: %f%n", perimeter);
+
+        Scanner scanner3 = new Scanner(System.in);
+        scanner3.useDelimiter("%n");
+        System.out.printf("Enter the length of your room%nEnter the width of your Room\n");
+
+        String length = scanner3.nextLine();
+        String width = scanner3.nextLine();
+
+        float lengthNum = Float.parseFloat(length);
+        float widthNum = Float.parseFloat(width);
+
+        System.out.printf("Length: %f%nWidth: %f%n", lengthNum, widthNum);
+        float area = widthNum*lengthNum;
+        System.out.printf("Area: %f%n", area);
+        float perimeter = (widthNum*2)+(lengthNum*2);
+        System.out.printf("Perimeter: %f%n", perimeter);
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,53 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 14;
+        String myString = "Killian Orion";
+//        char myString = "Killian Orion";
+//        String myString = 3.147;
+//        long myNumber;
+//        long myNumber = 3.14;
+//        long myNumber = 123L;
+//        long myNumber = 123;
+//        float myNumber = 3.14;
+        float myNumber = (float) 3.14;
+//        double myNumber = 3.14;
+//        String class = "Can't do this yo";
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        int x = 4;
+//        x = x + 5;
+        x += 5;
+        System.out.println(x);
+
+        int xx = 3;
+        int yy = 4;
+//        yy = yy * xx;
+        yy *= xx;
+        System.out.println(yy);
+
+        int xxx = 10;
+        int yyy = 2;
+//        xxx = xxx / yyy;
+//        yyy = yyy - xxx;
+        xxx /= yyy;
+        yyy -= xxx;
+        System.out.println(xxx);
+        System.out.println(yyy);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+        System.out.print("Hello World!");
+        System.out.println(" World!!");
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+    }
+}
